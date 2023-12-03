ebe957114bd9d93a787e036ab8757c353be2f100
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2023-12-03 21:14:27 +0100

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
-
+ const pokemonList = [
+  {
+      name: "bulbizarre",
+      imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+      name: "mew",
+  },
+];
 function PokemonCard() {
-    return ( <figure>
-       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-       <figcaption>Bulbasaur</figcaption>
+  const pokemon = pokemonList[1];
+
+  return (
+      <figure>
+          <figcaption>{pokemon.name}</figcaption>
+          {pokemon.imgSrc ? (
+              <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+              <p>???</p>
+          )}
       </figure>
-    );
-  }
-  
-  export default PokemonCard;
+  );
+}
+
+export default PokemonCard;
