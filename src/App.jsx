4c85033927ade614a80d8925ef530459d56b5935
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2023-12-04 10:34:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import PokemonCard from './components/PokemonCard'
-function App() {
-  return <div>
-    <PokemonCard/> 
-    </div>
-}
+import PokemonCard from "./components/PokemonCard"
 
+function App() {
+  const pokemonList = [
+    {
+      name: "bulbizarre",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[0];
+
+  return (
+    <div className="App">
+      <PokemonCard pokemon={pokemon} />
+    </div>
+  );
+}
 export default App;
