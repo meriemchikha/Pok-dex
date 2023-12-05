adb7d79570a8c4e1ed9e9c939925a1df2a88b957
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2023-12-05 23:08:37 +0100

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NavBar({ handlePreviousClick, handleNextClick }) {
+    return (
+        <div className="pokemon"> 
+ 
+        
+          <button onClick={handlePreviousClick}>Précédent</button>
+       
+        
+          <button onClick={handleNextClick}>Suivant</button>
+       
+       </div>
+    );
+  }
+  
+ 
+
+  export default NavBar;
