e6bb55e13a9e50459dcb5f77a8d8225ebc85146d
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2023-12-05 21:55:45 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,67 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
 import PokemonCard from "./components/PokemonCard"
+
+import { useState } from "react";
 
 function App() {
 
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+ 
   const [pokemonIndex, setPokemonIndex] = useState(0);
+  const selectedPokemon = pokemonList[pokemonIndex];
 
   const handlePreviousClick = () => {
     if (pokemonIndex > 0){
-      setPokemonIndex((pokemonIndex) => pokemonIndex - 1);
+      setPokemonIndex(pokemonIndex - 1);
     }
     
   };
 
   const handleNextClick = () => {
-    if (pokemonIndex < 0){
-      setPokemonIndex((pokemonIndex) => pokemonIndex + 1);
+    if (pokemonIndex < pokemonList.length - 1 ){
+      setPokemonIndex(pokemonIndex + 1);
     }
     
   };
 
   return (
-    <div>
-      <h1>{pokemonList[pokemonIndex]}</h1>
+    <div className="App">
+      <div className="pokemon"> 
+ 
       {pokemonIndex > 0 && (
         <button onClick={handlePreviousClick}>Précédent</button>
       )}
       {pokemonIndex < pokemonList.length - 1 && (
         <button onClick={handleNextClick}>Suivant</button>
       )}
+     </div>
+     <PokemonCard pokemon={selectedPokemon} />
     </div>
   );
 }
 
-const pokemonList = [
-  {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "charmander",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-    },
-    {
-      name: "squirtle",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "pikachu",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-export default App;
+
+export default App; 
