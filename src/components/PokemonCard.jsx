c061cfbaf91ff9f4790f76816f9afe4dcda41ddb
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2023-12-12 16:48:25 +0100

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,18 @@
-function PokemonCard(props) {
-    console.log(props);
-    const { pokemon } = props;
-    const { imgSrc, name } = pokemon;
-    return (
-        <figure className="pokemon-card">
-            <figcaption>{pokemon.name}</figcaption>
-            {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
-            ) : (
-                <p>???</p>
-            )}
-        </figure>
-    );
-}
+function PokemonCard({pokemon}){
 
+  
+
+    return <div>
+      {pokemon.map((p)=> (
+        
+        p.imgSrc ? (<> <img src={p.imgSrc}/> 
+        <p>{p.name}</p></>) : <p>{"???"}</p>
+        
+      )) }
+        
+    </div>
+  
+}
 
 export default PokemonCard;
 
