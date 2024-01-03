867eafe088d7e601ddecf505be7a5bfe8a118c9e
meriemchikha <148758538+meriemchikha@users.noreply.github.com>
2024-01-03 09:55:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,137 @@
-import PokemonCard from "./components/PokemonCard"
-
-import { useState } from "react";
-
+import PokemonCard from "./components/PokemonCard";
 import NavBar from "./components/NavBar";
-function App() {
+import { useState } from "react";
+import "../src/App.css";
 
+function App() {
   const pokemonList = [
     {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
- 
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      id: 1,
+    },
+    {
+      name: "Charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      id: 4,
+    },
+    {
+      name: "Squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      id: 7,
+    },
+    {
+      name: "Rattata",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png",
+      id: 19,
+    },
+    {
+      name: "Pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      id: 25,
+    },
+    {
+      name: "Oddish",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/43.png",
+      id: 43,
+    },
+    {
+      name: "Psyduck",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/54.png",
+      id: 54,
+    },
+    {
+      name: "Golduck",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/55.png",
+      id: 55,
+    },
+    {
+      name: "Magnemite",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/81.png",
+      id: 81,
+    },
+    {
+      name: "Farfetch'd",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/83.png",
+      id: 83,
+    },
+    {
+      name: "Gastly",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/92.png",
+      id: 92,
+    },
+    {
+      name: "Haunter",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/93.png",
+      id: 93,
+    },
+    {
+      name: "Drowzee",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/96.png",
+      id: 96,
+    },
+    {
+      name: "Lapras",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/131.png",
+      id: 131,
+    },
+    {
+      name: "Porygon",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/137.png",
+      id: 137,
+    },
+    {
+      name: "Mew",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+      id: 151,
+    },
+    {
+      name: "Crobat",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/169.png",
+      id: 169,
+    },
+    {
+      name: "Sudowoodo",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/185.png",
+      id: 185,
+    },
+  ];
+
   const [pokemonIndex, setPokemonIndex] = useState(0);
-  const selectedPokemon = pokemonList[pokemonIndex];
 
-  const handlePreviousClick = () => {
-    if (pokemonIndex > 0){
-      setPokemonIndex(pokemonIndex - 1);
-    }
-    
-  };
-
-  const handleNextClick = () => {
-    if (pokemonIndex < pokemonList.length - 1 ){
-      setPokemonIndex(pokemonIndex + 1);
-    }
-    
+  const handlePokemonClick = (index) => {
+    setPokemonIndex(index);
   };
 
   return (
-    <div className="App">
-       <NavBar
-         handlePreviousClick= {handlePreviousClick}
-         handleNextClick= {handleNextClick}
-      
-       />
-     <PokemonCard pokemon={selectedPokemon} />
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <NavBar
+        pokemonList={pokemonList}
+        pokemonIndex={pokemonIndex}
+        setPokemonIndex={setPokemonIndex}
+        onPokemonClick={handlePokemonClick}
+      />
     </div>
-
-  )
- 
+  );
 }
-
 
 export default App;
